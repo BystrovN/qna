c45b613fa4a5ebb7d33fec8169900b1a9f0136f3
 Никита Быстров <hallnik96@gmail.com>
2024-03-27 00:22:00 +0300

    feat: lesson_6 0.2 ask question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,19 +1,27 @@
 class QuestionsController < ApplicationController
+  before_action :authenticate_user!, except: %i[index show]
+
   def index
     @questions = Question.all
   end
 
-  def show; end
+  def show
+    question
+  end
 
-  def new; end
+  def new
+    @question = Question.new
+  end
 
-  def edit; end
+  def edit
+    question
+  end
 
   def create
     @question = Question.new(question_params)
 
     if @question.save
-      redirect_to @question
+      redirect_to @question, notice: 'Your question successfully created'
     else
       render :new
     end
