505d2256ccbd9ac1fa545fd675c1472452c2c48f
 Никита Быстров <hallnik96@gmail.com>
2024-07-04 02:19:17 +0300

    feat: lesson_12 0.1 oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,23 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable,
+         omniauth_providers: [:github]
 
   has_many :answers, dependent: :destroy
   has_many :questions, dependent: :destroy
   has_many :rewards, dependent: :nullify
+  has_many :authorizations, dependent: :destroy
+
+  def self.find_for_oauth(auth)
+    FindForOauth.new(auth).call
+  end
 
   def author_of?(resource)
     resource.user_id == id
+  end
+
+  def create_authorization(auth)
+    authorizations.create(provider: auth.provider, uid: auth.uid)
   end
 end
